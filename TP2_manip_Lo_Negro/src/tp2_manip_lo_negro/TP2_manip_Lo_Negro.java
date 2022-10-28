e38ba23f3fd234dabd3cf8738fbfc84a7c889d59
Dorian Lo_Negro <dorian.lonegro@epfedu.fr>
2022-10-28 16:20:45 +0200

    Creation d'un tableau et remplissage de valeur avec une boucle

--- TP2_manip_Lo_Negro/src/tp2_manip_lo_negro/TP2_manip_Lo_Negro.java
+++ TP2_manip_Lo_Negro/src/tp2_manip_lo_negro/TP2_manip_Lo_Negro.java
@@ -26,9 +26,22 @@ public class TP2_manip_Lo_Negro {
         System.out.println("nb de calories de Assiette 3 : " +
         assiette3.nbCalories) ;//assiette 2 et 3 ont bien le même nombre de calorie
         int changement;
+        
+        assiette2.nbCalories += 300;
+        System.out.println(assiette2.nbCalories);
+        System.out.println(assiette3.nbCalories);
         changement = assiette1.nbCalories;
         assiette1.nbCalories=assiette2.nbCalories;
         assiette2.nbCalories=changement;// changement des valeurs assiette1 = assiette 2 et inversément 
+        
+        //Moussaka assiette666 = assiette1 ; Moussaka n'est pas convertible en tartiflette
+        //Moussaka assiette667 = new Tartiflette() ; Moussaka n'a aucun rapport avec tartiflette donc cette ligne ne fonctionne pas
+        
+        Moussaka TableauM [] = new Moussaka[10];
+        for (int i=0; i < TableauM.length; i++){
+            TableauM[i]=new Moussaka(i*i*10);
+            
+        }
         
     }
     
