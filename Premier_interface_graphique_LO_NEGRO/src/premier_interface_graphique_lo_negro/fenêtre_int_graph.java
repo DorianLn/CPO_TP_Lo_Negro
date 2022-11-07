3fdfd8d4423b33bfe77b70294a36d3ac39440fd1
Dorian Lo_Negro <dorian.lonegro@epfedu.fr>
2022-11-07 12:51:35 +0100

    affichage salaire augmenté

--- Premier_interface_graphique_LO_NEGRO/src/premier_interface_graphique_lo_negro/fenêtre_int_graph.java
+++ Premier_interface_graphique_LO_NEGRO/src/premier_interface_graphique_lo_negro/fenêtre_int_graph.java
@@ -15,6 +15,10 @@ public class fenêtre_int_graph extends javax.swing.JFrame {
      */
     public fenêtre_int_graph() {
         initComponents();
+        System.out.println("fenêtre créée"); //ici que l'on peut écrire
+        affichage.setVisible(false);
+        affichage_nom.setVisible(false);
+       
     }
 
     /**
@@ -26,21 +30,159 @@ public class fenêtre_int_graph extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        msgbienvenue = new javax.swing.JLabel();
+        msgaurevoir = new javax.swing.JLabel();
+        bouttonvalider = new javax.swing.JButton();
+        rentrer_nom = new javax.swing.JTextField();
+        nom = new javax.swing.JLabel();
+        affichage = new javax.swing.JLabel();
+        affichage_nom = new javax.swing.JLabel();
+        salaire = new javax.swing.JLabel();
+        salaire_entrer = new javax.swing.JTextField();
+        bouton_salaire = new javax.swing.JButton();
+        salaire_up = new javax.swing.JLabel();
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        jLabel1.setText("jLabel1");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        msgbienvenue.setText("hola");
+
+        bouttonvalider.setBackground(new java.awt.Color(255, 0, 0));
+        bouttonvalider.setText("valider");
+        bouttonvalider.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bouttonvaliderActionPerformed(evt);
+            }
+        });
+
+        rentrer_nom.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rentrer_nomActionPerformed(evt);
+            }
+        });
+
+        nom.setText("entrer votre nom");
+
+        affichage.setText("votre nom:");
+
+        affichage_nom.setText("Nom");
+
+        salaire.setText("entrez votre salaire : ");
+
+        bouton_salaire.setText("augmenter votre salaire");
+        bouton_salaire.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bouton_salaireActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(346, 346, 346)
+                .addComponent(msgbienvenue, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(88, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(affichage, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(nom))
+                            .addGap(18, 18, 18)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(rentrer_nom, javax.swing.GroupLayout.DEFAULT_SIZE, 277, Short.MAX_VALUE)
+                                .addComponent(affichage_nom, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGap(216, 216, 216))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(salaire_up, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(salaire)
+                                    .addGap(18, 18, 18)
+                                    .addComponent(salaire_entrer, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGap(42, 42, 42)
+                            .addComponent(bouton_salaire)
+                            .addGap(160, 160, 160)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(msgaurevoir, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(280, 280, 280)
+                        .addComponent(bouttonvalider)
+                        .addGap(25, 25, 25))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(msgbienvenue, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(nom)
+                    .addComponent(rentrer_nom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(21, 21, 21)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(affichage)
+                    .addComponent(affichage_nom))
+                .addGap(29, 29, 29)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(salaire)
+                    .addComponent(salaire_entrer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bouton_salaire))
+                .addGap(18, 18, 18)
+                .addComponent(salaire_up, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(bouttonvalider, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(msgaurevoir, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(19, 19, 19))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bouttonvaliderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bouttonvaliderActionPerformed
+        // TODO add your handling code here:
+        String a = rentrer_nom.getText();
+        msgaurevoir.setText("ciao");//endroit pour faire une action avec le bouton 
+        affichage.setVisible(true);
+        affichage_nom.setText(a);
+        affichage_nom.setVisible(true);
+        
+    }//GEN-LAST:event_bouttonvaliderActionPerformed
+
+    private void rentrer_nomActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rentrer_nomActionPerformed
+        // TODO add your handling code here:
+        
+        
+    }//GEN-LAST:event_rentrer_nomActionPerformed
+
+    private void bouton_salaireActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bouton_salaireActionPerformed
+        // TODO add your handling code here:
+        String salaire = salaire_entrer.getText();
+        int salaire_entier = Integer.parseInt(salaire);
+        salaire_entier ++;
+        salaire_up.setText(salaire_entier + "");
+        
+        
+        
+    }//GEN-LAST:event_bouton_salaireActionPerformed
 
     /**
      * @param args the command line arguments
@@ -75,8 +217,29 @@ public class fenêtre_int_graph extends javax.swing.JFrame {
                 new fenêtre_int_graph().setVisible(true);
             }
         });
+        
+        
+        
+        
     }
+    
+    
+    
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel affichage;
+    private javax.swing.JLabel affichage_nom;
+    private javax.swing.JButton bouton_salaire;
+    private javax.swing.JButton bouttonvalider;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel msgaurevoir;
+    private javax.swing.JLabel msgbienvenue;
+    private javax.swing.JLabel nom;
+    private javax.swing.JTextField rentrer_nom;
+    private javax.swing.JLabel salaire;
+    private javax.swing.JTextField salaire_entrer;
+    private javax.swing.JLabel salaire_up;
     // End of variables declaration//GEN-END:variables
 }
