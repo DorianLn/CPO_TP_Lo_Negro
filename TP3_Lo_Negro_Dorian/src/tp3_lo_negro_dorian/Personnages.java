e199338beab5756b8d137e89c9ff9fab6eb3f1e6
Dorian Lo_Negro <dorian.lonegro@epfedu.fr>
2022-11-15 15:28:43 +0100

    Création d'un constructeur dans personnage

--- TP3_Lo_Negro_Dorian/src/tp3_lo_negro_dorian/Personnages.java
+++ TP3_Lo_Negro_Dorian/src/tp3_lo_negro_dorian/Personnages.java
@@ -12,5 +12,15 @@ public class Personnages {
     String nom; 
     int niveau_de_vie;
     
+    public Personnages (String Nom, int nv){//constructeur
+        nom = Nom;
+        niveau_de_vie = nv;
+    }
     
+    
+    @Override// création du string tostring
+    public String toString () {
+    return "niveaude vie : " + niveau_de_vie + "nom personnage" + nom;
+    
+    }
 }
