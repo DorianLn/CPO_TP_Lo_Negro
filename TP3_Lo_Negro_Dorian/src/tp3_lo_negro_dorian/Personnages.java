d4e561a2973dd00068d07bf0eb8d8d8e0d11d302
Dorian Lo_Negro <dorian.lonegro@epfedu.fr>
2022-11-15 15:25:08 +0100

    Création classe personnages

---
+++ TP3_Lo_Negro_Dorian/src/tp3_lo_negro_dorian/Personnages.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_lo_negro_dorian;
+
+/**
+ *
+ * @author doria
+ */
+public class Personnages {
+    String nom; 
+    int niveau_de_vie;
+    
+    
+}
