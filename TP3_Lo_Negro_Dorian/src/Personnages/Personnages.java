77814f821243be874739f657883d2028739215ff
Dorian Lo_Negro <dorian.lonegro@epfedu.fr>
2022-11-15 17:00:48 +0100

    Ajout d'un tableau dynamique dans personnage pour les armes des perso

--- TP3_Lo_Negro_Dorian/src/Personnages/Personnages.java
+++ TP3_Lo_Negro_Dorian/src/Personnages/Personnages.java
@@ -4,18 +4,39 @@
  */
 package Personnages;
 
+import Armes.Armes;
+import java.util.ArrayList;
+
 /**
  *
  * @author doria
  */
 public class Personnages {
+    public ArrayList<Armes> nb_arme = new ArrayList<Armes>();
     String nom; 
     int niveau_de_vie;
+    int caseT;
     
-    public Personnages (String Nom, int nv){//constructeur
+    
+    public Personnages (String Nom, int nv ){//constructeur
         nom = Nom;
         niveau_de_vie = nv;
+        
     }
+    
+   
+    
+    public void setArmeperso(Armes arme ){//change la valeur
+        caseT= nb_arme.size();
+        if (caseT<=5){
+            nb_arme.add(arme);
+        }
+        
+        
+        
+    }
+    
+    
     
     
     @Override// création du string tostring
